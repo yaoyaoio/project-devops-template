a6abb18e49a9a02d8044958661ca2ce2fb979b7a
yaoyao <i@yaoyao.io>
2022-11-22 23:40:44 +0800

    fix

--- go-sample/cmd/sample/main.go
+++ go-sample/cmd/sample/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Hello World")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
