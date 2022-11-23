71b26bacfc0bc6971fcf12f832a6556862ee703d
yaoyao <i@yaoyao.io>
2022-11-23 19:25:50 +0800

    update go files

--- go-sample/cmd/sample/main.go
+++ go-sample/cmd/sample/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"go-sample/pkg/service"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	service.NewService()
 }
