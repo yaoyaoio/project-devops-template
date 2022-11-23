0fe962160c877b69cea0ef48924ae236a61341ec
yaoyao <i@yaoyao.io>
2022-11-23 22:26:16 +0800

    update mvn files

---
+++ mvn-sample/README.MD
@@ -0,0 +1,1 @@
+# mvn-sample
---
+++ mvn-sample/src/main/java/io/yaoyao/sample/MvnSampleApplication.java
@@ -0,0 +1,13 @@
+package io.yaoyao.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnSampleApplication.class, args);
+	}
+}
+
